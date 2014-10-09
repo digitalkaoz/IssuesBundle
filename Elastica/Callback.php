8ce3515599f71166e982c2518980d18fc2275231
Robert Schönthal <caziel@gmx.net>
2014-10-09 17:51:21 +0200

    fixed serialization

---
+++ Elastica/Callback.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rs\IssuesBundle\Elastica;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
+
+
+/**
+ * Callback
+ * @author Robert Schönthal <robert.schoenthal@gmail.com>
+ */
+class Callback extends \FOS\ElasticaBundle\Serializer\Callback
+{
+    public function serialize($object)
+    {
+        $context = $this->serializer instanceof SerializerInterface ? SerializationContext::create()->enableMaxDepthChecks()->setSerializeNull(true) : array();
+
+        if ($this->groups) {
+            $context->setGroups($this->groups);
+        }
+
+        if ($this->version) {
+            $context->setVersion($this->version);
+        }
+
+        return $this->serializer->serialize($object, 'json', $context);
+    }
+
+} 
